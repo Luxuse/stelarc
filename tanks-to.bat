55130b20da4a45ee20db8103d417171db51f24b7
luxuse <137567329+Luxuse@users.noreply.github.com>
2025-05-31 00:11:02 +0200

    thx

---
+++ tanks-to.bat
@@ -0,0 +1,10 @@
+@echo off
+echo thanks to everyone
+echo thanks to https://fileforums.com/
+echo big thanks to Razor12911 ,BLACKFIRE69, ProFrager
+echo thanks to Bulat Ziganshin
+echo thanks to Masquerade
+echo thanks to gpicxel for testing
+echo thanks to https://encode.su/
+echo thanks to fitgirl, DODI
+pause
